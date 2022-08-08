ef5ddcbb313efc5c8a17a766614526c52b314129
sergey <sergey.trutsin@gmail.com>
2022-08-08 17:16:18 +0300

    validation

--- src/main/java/com/trutsyn/project/controller/UserController.java
+++ src/main/java/com/trutsyn/project/controller/UserController.java
@@ -2,9 +2,11 @@ package com.trutsyn.project.controller;
 
 import com.trutsyn.project.dao.UserDao;
 import com.trutsyn.project.models.User;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.jws.WebParam;
@@ -39,7 +41,10 @@ public class UserController {
     }
 
     @PostMapping()
-    public String create(@ModelAttribute("user") User user) {
+    public String create(@ModelAttribute("user") @Valid User user, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "people/new";
+        }
         userDao.save(user);
         return "redirect:/users";
     }
@@ -51,7 +56,11 @@ public class UserController {
     }
 
     @PatchMapping("/{id}")
-    public String update(@ModelAttribute("user") User user, @PathVariable("id") int id) {
+    public String update(@ModelAttribute("user") @Valid User user, BindingResult bindingResult,
+                         @PathVariable("id") int id) {
+        if (bindingResult.hasErrors()) {
+            return "people/edit";
+        }
         userDao.update(id, user);
         return "redirect:/users";
     }
--- src/main/java/com/trutsyn/project/models/User.java
+++ src/main/java/com/trutsyn/project/models/User.java
@@ -1,11 +1,20 @@
 package com.trutsyn.project.models;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
 /**
  * @author Sergey Trutsyn 08.08.2022
  */
 public class User {
     private int id;
+
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 30, message = "Name should be between 2 and 30")
     private String name;
+
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 30, message = "Name should be between 2 and 30")
     private String surname;
 
     public User(int id, String name, String surname) {
